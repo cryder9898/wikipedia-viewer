0ae3e3da7cb80a38d7f6768b173320215e5d6162
Christopher Ryder <cryder9898@gmail.com>
2017-07-13 09:34:57 -0400

    fixed app for mobile

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
   "use strict";
-  
+
   var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&namespace=0&limit=10&origin=*&search=';
 
   function setWikiResults(data) {
     let list;
-    
+
     list = data.map(function (x, i) {
       let titles = data[1];
       let extracts = data[2];
       let urls = data[3];
-      
+
       return '<li><a href="' + urls[i] + '" target="_blank"><h3>' + titles[i] + '</h3>' + extracts[i] + '</a></li>';
     }).join('');
-    
+
     $('#wikilist').append(list);
   }
 
   $('#searchButton').click(function() {
-    let query = $('#search').val();
+    const query = $('#search').val();
     if (query !== '' && typeof query !== 'undefined') {
       $.getJSON(url + encodeURIComponent(query), function(data) {
         $('#wikilist').empty();
@@ -26,5 +26,5 @@ $(document).ready(function() {
       });
     }
   });
-  
+
 });
